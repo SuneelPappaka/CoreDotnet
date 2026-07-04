b55d13a43a3c74e4e0aab1bff08903d72eab79e9
SuneelPappaka <pappakasuneel123@gmail.com>
2026-07-05 03:14:36 +0530

    Display Records in Cart View

---
+++ Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using CoreDotnet.Data;
+using CoreDotnet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace CoreDotnet.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public CartController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext= applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            var userid = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var cartItems = _applicationDbContext.CartItem.Where(x=>x.UserId==userid).Include(x=>x.Product).ToList();
+            return View(cartItems);
+        }
+    }
+}
