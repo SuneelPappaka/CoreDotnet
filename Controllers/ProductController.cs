029b83071872f727d9c2956a05edd407547290ba
SuneelPappaka <pappakasuneel123@gmail.com>
2026-07-04 12:05:03 +0530

    #42 Display All Products in Index View

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using CoreDotnet.Data;
+using CoreDotnet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDotnet.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public ProductController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Product> _products = _applicationDbContext.Products.ToList();
+            return View(_products);
+        }
+    }
+}
+
